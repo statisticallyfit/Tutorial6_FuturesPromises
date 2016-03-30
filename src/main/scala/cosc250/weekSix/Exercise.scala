1a54a6619098bbafb6d20073f164c25d9c79c7c0
William Billingsley <wbilling@une.edu.au>
2016-03-30 16:39:52 +1100

    Added a solution

--- src/main/scala/cosc250/weekSix/Exercise.scala
+++ src/main/scala/cosc250/weekSix/Exercise.scala
@@ -21,13 +21,13 @@ object Exercise {
   /**
     * Just complete this promise with a number. Have a look at what the test is doing
     */
-  def completeMyPromise(p:Promise[Int]:Unit = ???
+  def completeMyPromise(p:Promise[Int]):Unit = p.success(1)
 
   /**
     * I'm going to give you a Future[Int]. Double it and return it.
     * Have a look at what the test is doing
     */
-  def doubleMyFuture(p:Future[Int]):Future[Int] = ???
+  def doubleMyFuture(p:Future[Int]):Future[Int] = p.map(_ * 2)
 
 
   /**
@@ -39,7 +39,12 @@ object Exercise {
     *
     * Don't use isComplete.
     */
-  def compareMyFutureStrings(fs1:Future[String], fs2:Future[String]):Future[Int] = ???
+  def compareMyFutureStrings(fs1:Future[String], fs2:Future[String]):Future[Int] = {
+    for {
+      s1 <- fs1
+      s2 <- fs2
+    } yield s1.zip(s2).count({ case (char1, char2) => char1 == char2 })
+  }
 
   /**
     * Here's an example of parsing a JSON string
@@ -94,7 +99,17 @@ object Exercise {
     *
     * Get the file http://turing.une.edu.au/~cosc250/lectures/cosc250/second.json and extract the name from the JSON
     */
-  def secondName():Future[String] = ???
+  def secondName():Future[String] = {
+    for {
+      contents <- wsClient
+        .url("http://turing.une.edu.au/~cosc250/lectures/cosc250/second.json")
+        .get()
+        .map(_.body)
+    } yield {
+      val json = Json.parse(contents)
+      (json \ "name").as[String]
+    }
+  }
 
   /**
     * Your second challenge...
@@ -104,7 +119,26 @@ object Exercise {
     * Parse them each as JSON
     * and case insensitively see how many characters are in common in the two names...
     */
-  def nameCharactersInCommon(url1:String, url2:String):Future[Int] = ???
+  def nameCharactersInCommon(url1:String, url2:String):Future[Int] = {
+    for {
+      first <- wsClient
+        .url(url1)
+        .get()
+        .map(_.body)
+      second <- wsClient
+        .url(url2)
+        .get()
+        .map(_.body)
+    } yield {
+      val json1 = Json.parse(first)
+      val json2 = Json.parse(second)
+      val name1 = (json1 \ "name").as[String]
+      val name2 = (json2 \ "name").as[String]
+
+      name1.zip(name2).count({ case (char1, char2) => char1 == char2 })
+    }
+
+  }
 
 
 }
