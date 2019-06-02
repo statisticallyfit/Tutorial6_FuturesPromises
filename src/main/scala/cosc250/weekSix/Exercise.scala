03a62dd99b9b7e4b5f760368cb900e5f3295fea8
BabitzdaBrave <amg.vintila@gmail.com>
2019-06-02 15:32:36 -0400

    done T6

--- src/main/scala/cosc250/weekSix/Exercise.scala
+++ src/main/scala/cosc250/weekSix/Exercise.scala
@@ -21,13 +21,18 @@ object Exercise {
 	/**
 	  * Just complete this promise with a number. Have a look at what the test is doing
 	  */
-	def completeMyPromise(p:Promise[Int]):Unit = ???
+	def completeMyPromise(p:Promise[Int]):Unit = {
+		//given the promise, we can substitute a value inside a future
+		p.success(23)
+	}
 
 	/**
 	  * I'm going to give you a Future[Int]. Double it and return it.
 	  * Have a look at what the test is doing
 	  */
-	def doubleMyFuture(p:Future[Int]):Future[Int] = ???
+	def doubleMyFuture(p:Future[Int]):Future[Int] = {
+		p.map(value => value * 2)
+	}
 
 
 	/**
@@ -39,7 +44,12 @@ object Exercise {
 	  *
 	  * Don't use isComplete.
 	  */
-	def compareMyFutureStrings(fs1:Future[String], fs2:Future[String]):Future[Int] = ???
+	def compareMyFutureStrings(fs1:Future[String], fs2:Future[String]):Future[Int] = {
+		for {
+			s1 <- fs1
+			s2 <- fs2
+		} yield s1.toList.zip(s2.toList).count({ case (char1, char2) => char1 == char2 })
+	}
 
 	/**
 	  * Here's an example of parsing a JSON string
@@ -94,7 +104,30 @@ object Exercise {
 	  *
 	  * Get the file http://turing.une.edu.au/~cosc250/lectures/cosc250/second.json and extract the name from the JSON
 	  */
-	def secondName():Future[String] = ???
+	def secondName():Future[String] = {
+		for {
+			contents <- wsClient
+				.url("http://turing.une.edu.au/~cosc250/lectures/cosc250/second.json")
+				.get()
+				.map(_.body)
+		} yield {
+			val json: JsValue = Json.parse(contents)
+			(json \ "name").as[String]
+		}
+		/*val strFuture: Future[String] = wsClient
+			.url("http://turing.une.edu.au/~cosc250/lectures/cosc250/second.json")
+			.get().map(_.body)
+
+		val jsFuture: Future[JsValue] = for {
+			jsonStr <- strFuture
+		} yield Json.parse(jsonStr)
+
+		val name: Future[String] = jsFuture.map(js => (js \ "name").as[String])
+		name*/
+	}
+	//TODO what is the difference between putting it all in one future as above (live code) and putting
+	//TODO it in separate futures, like in the comments?
+
 
 	/**
 	  * Your second challenge...
@@ -104,7 +137,31 @@ object Exercise {
 	  * Parse them each as JSON
 	  * and case insensitively see how many characters are in common in the two names...
 	  */
-	def nameCharactersInCommon(url1:String, url2:String):Future[Int] = ???
+	def nameCharactersInCommon(url1:String, url2:String):Future[Int] = {
+		/*val (n1, n2): (Future[String], Future[String]) = *///TODO why doesn't this work?
+		for {
+			contents1 <- wsClient
+				.url(url1)
+				.get()
+				.map(_.body)
+			contents2 <- wsClient
+				.url(url2)
+				.get()
+				.map(_.body)
+		} yield {
+			//parse them each as json while in the same future
+			val json1: JsValue = Json.parse(contents1)
+			val json2: JsValue = Json.parse(contents2)
+			val name1: String = (json1 \ "name").as[String]
+			val name2: String = (json2 \ "name").as[String]
+
+			//(name1, name2)
+			name1.zip(name2).count({case(letter1, letter2) => letter1 == letter2 })
+		}
+
+		//exiting the future and now count how many strings equal.
+		//compareMyFutureStrings(n1, n2)
+	}
 
 
 }
